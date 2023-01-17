fd8f9a9be8874ee2b1c58f6f78ce7591a1fe8265
Krzysztof <76277851+KrzysztofKan@users.noreply.github.com>
2023-01-17 15:26:31 +0100

    Add files via upload

---
+++ Sokob.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+#include "Sokoban.h"
+
+//Gra Sokoban
+//
+//Pola :
+//# - sciana
+//. - podloga
+//@ - gracz
+//o - skrzynia
+//_ - pole docelowa
+//
+//Przykładowy poziom :
+//#######
+//#.....#
+//#.@o_.#
+//#.....#
+//#######
+//
+//Zasady:
+//1. Gracz może wykonywać kroki w kierunkach góra, dół, lewo, prawo.
+//2. Gracz może poruszać się po polach '.' i '_'.
+//3. Wejście na pole ze skrzynią('o') jest wykonalne tylko jeśli następne pole w tym samym kierunku jest puste('.' lub '_').Skrzynia jest przesuwana na to pole.
+//4. Jeśli na wszystkich polach docelowych znajduje się skrzynia to gracz wygrywa.
+
+
+
+int main() {
+	Sokoban sok;
+	sok.graj();
+}
---
+++ Sokoban.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <istream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+char level1[5][7] = {	'#','#','#','#','#','#','#',
+						'#','.','.','.','.','.','#',
+						'#','.','@','o','_','.','#',
+						'#','.','.','.','.','.','#',
+						'#','#','#','#','#','#','#'
+};
+
+class Aktor {
+public:
+	Aktor(const char& z) {
+		zna = z;
+	}
+	char Znak() {
+		return zna;
+	}
+	void SetPos(int x, int y) {
+		posX = x;
+		posY = y;
+	}
+	int getx() {
+		return posX;
+	}
+	int gety() {
+		return posY;
+	}
+	void moveUP() {
+		posX--;
+	}
+	void moveDOWN() {
+		posX++;
+	}
+	void moveR(){
+		posY++;
+	}
+	void moveL(){
+		posY--;
+	}
+private:
+	char zna;
+	int posX = 0;
+	int posY = 0;
+};
+
+class Sokoban{
+public:
+	void graj();
+	void wypisz();
+	int znajdzX(char z);
+	int znajdzY(char z);
+private:
+	Aktor gracz = Aktor('@');
+	Aktor skrz = Aktor('o');
+	Aktor cel = Aktor('_');
+};
+
+void Sokoban::graj() {
+	string choice;
+	wypisz();
+	gracz.SetPos(znajdzX(gracz.Znak()), znajdzY(gracz.Znak()));
+	skrz.SetPos(znajdzX(skrz.Znak()), znajdzY(skrz.Znak()));
+	cel.SetPos(znajdzX(cel.Znak()), znajdzY(cel.Znak()));
+	while (choice != "x"){
+		cin >> choice;
+		if (choice == "w"){
+			level1[gracz.getx()][gracz.gety()] = '.';
+			level1[gracz.getx()-1][gracz.gety()] = gracz.Znak();
+			gracz.moveUP();
+			wypisz();
+		}
+		if (choice == "s") {
+			level1[gracz.getx()][gracz.gety()] = '.';
+			level1[gracz.getx() + 1][gracz.gety()] = gracz.Znak();
+			gracz.moveDOWN();
+			wypisz();
+		}
+		if (choice == "a") {
+			level1[gracz.getx()][gracz.gety()] = '.';
+			level1[gracz.getx()][gracz.gety() - 1] = gracz.Znak();
+			gracz.moveL();
+			wypisz();
+		}
+		if (choice == "d") {
+			level1[gracz.getx()][gracz.gety()] = '.';
+			level1[gracz.getx()][gracz.gety() + 1] = gracz.Znak();
+			gracz.moveR();
+			wypisz();
+		}
+	}
+}
+
+void Sokoban::wypisz() {
+	for (int x = 0; x < 5; x++) {
+		for (int y = 0; y < 7; y++) {
+			cout << level1[x][y];
+		}
+		cout << endl;
+	}
+}
+
+int Sokoban::znajdzX(char z){
+	for (int x = 0; x < 5; x++) {
+		for (int y = 0; y < 7; y++) {
+			if (level1[x][y] == z){
+				return x;
+			} 
+		}
+	}
+}
+
+int Sokoban::znajdzY(char z) {
+	for (int x = 0; x < 5; x++) {
+		for (int y = 0; y < 7; y++) {
+			if (level1[x][y] == z) {
+				return y;
+			}
+		}
+	}
+}
