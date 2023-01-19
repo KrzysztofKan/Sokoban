f8056c1f77c19c2ba29dfacb1792b47a7b0a9446
KrzysztofKan <krzysztof_kantoch@hotmail.com>
2023-01-19 13:23:44 +0100

    Added logic for movin crate

--- Sokoban.h
+++ Sokoban.h
@@ -22,6 +22,9 @@ public:
 	char Znak() {
 		return zna;
 	}
+	char pod() {
+		return pd;
+	}
 	void SetPos(int x, int y) {
 		posX = x;
 		posY = y;
@@ -46,6 +49,7 @@ public:
 	}
 private:
 	char zna;
+	char pd = '.';
 	int posX = 0;
 	int posY = 0;
 };
@@ -72,35 +76,70 @@ void Sokoban::graj() {
 		cin >> choice;
 		if (choice == "w"){
 			if (level1[gracz.getx()-1][gracz.gety()] == '.'){
-				level1[gracz.getx()][gracz.gety()] = '.';
+				level1[gracz.getx()][gracz.gety()] = gracz.pod();
 				level1[gracz.getx() - 1][gracz.gety()] = gracz.Znak();
 				gracz.moveUP();
+			}
+			if (level1[gracz.getx() - 1][gracz.gety()] == 'o'){
+				if (level1[skrz.getx() - 1][skrz.gety()] == '.'){
+					level1[skrz.getx() - 1][skrz.gety()] = skrz.Znak();
+					level1[gracz.getx() - 1][gracz.gety()] = gracz.Znak();
+					level1[gracz.getx()][gracz.gety()] = gracz.pod();
+					gracz.moveUP();
+					skrz.moveUP();
+				}
 			}
 			wypisz();
 		}
 		if (choice == "s") {
 			if (level1[gracz.getx() + 1][gracz.gety()] == '.'){
-				level1[gracz.getx()][gracz.gety()] = '.';
+				level1[gracz.getx()][gracz.gety()] = gracz.pod();
 				level1[gracz.getx() + 1][gracz.gety()] = gracz.Znak();
 				gracz.moveDOWN();
+			}
+			if (level1[gracz.getx() + 1][gracz.gety()] == 'o') {
+				if (level1[skrz.getx() + 1][skrz.gety()] == '.') {
+					level1[skrz.getx() + 1][skrz.gety()] = skrz.Znak();
+					level1[gracz.getx() + 1][gracz.gety()] = gracz.Znak();
+					level1[gracz.getx()][gracz.gety()] = gracz.pod();
+					gracz.moveDOWN();
+					skrz.moveDOWN();
+				}
 			}
 			wypisz();
 		}
 		if (choice == "a") {
 			if (level1[gracz.getx()][gracz.gety() - 1] == '.'){
-				level1[gracz.getx()][gracz.gety()] = '.';
+				level1[gracz.getx()][gracz.gety()] = gracz.pod();
 				level1[gracz.getx()][gracz.gety() - 1] = gracz.Znak();
 				gracz.moveL();
+			}
+			if (level1[gracz.getx()][gracz.gety()-1] == 'o') {
+				if (level1[skrz.getx()][skrz.gety() - 1] == '.') {
+					level1[skrz.getx()][skrz.gety() - 1] = skrz.Znak();
+					level1[gracz.getx()][gracz.gety() - 1] = gracz.Znak();
+					level1[gracz.getx()][gracz.gety()] = gracz.pod();
+					gracz.moveL();
+					skrz.moveL();
+				}
 			}
 			wypisz();
 		}
 		if (choice == "d") {
 			if (level1[gracz.getx()][gracz.gety() + 1] == '.'){
-				level1[gracz.getx()][gracz.gety()] = '.';
+				level1[gracz.getx()][gracz.gety()] = gracz.pod();
 				level1[gracz.getx()][gracz.gety() + 1] = gracz.Znak();
 				gracz.moveR();
 			}
-
+			if (level1[gracz.getx()][gracz.gety() + 1] == 'o') {
+				if (level1[skrz.getx()][skrz.gety() + 1] == '.') {
+					level1[skrz.getx()][skrz.gety() + 1] = skrz.Znak();
+					level1[gracz.getx()][gracz.gety() + 1] = gracz.Znak();
+					level1[gracz.getx()][gracz.gety()] = gracz.pod();
+					gracz.moveR();
+					skrz.moveR();
+				}
+			}
 			wypisz();
 		}
 	}
